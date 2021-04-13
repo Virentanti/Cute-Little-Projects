a949eb6ce84441810bd0c0738ffe5f0aa8bf5254
Viren Tanti <Virentanti16@gmail.com>
2021-04-13 12:43:06 +0530

    improved code quality

--- dice game(multiplayer).py
+++ dice game(multiplayer).py
@@ -5,6 +5,32 @@ player2_money=1000
 
 Player1= str(input('Player 1 Please enter your name '))
 Player2= str(input('Player 2 Please enter your name '))
+
+def money():
+    print(f'{Player1} Your money is {player1_money}')
+    print(f'{Player2} Your money is {player2_money}')
+
+def p1_won():
+    print(f'Congratulations {Player1}! You won')
+    print(f'Sorry {Player2}! You lost')
+    player1_money+=bet1
+    player2_money-=bet2
+
+def p2_won():
+    print(f'Congratulations {Player2}! You won')
+    print(f'Sorry {Player1}! You lost')
+    player1_money-=bet1
+    player2_money+=bet2
+
+def Both_won():
+    print(f'Congratulations {Player1} and {Player2}! You both won')
+    player1_money+=bet1
+    player2_money+=bet2
+
+def nobody_won():
+    print(f'Sorry {Player1} and {Player2}! You both lost')
+    player1_money-=bet1
+    player2_money-=bet2
 def game():
     global player1_money, player2_money, Player1,Player2
     for i in range(5):
@@ -17,31 +43,17 @@ def game():
                 if bet1<=player1_money and bet2<=player2_money:
                     dice=random.randrange(1,6)
                     if guess1==dice and guess2!=dice:
-                        print(f'Congratulations {Player1}! You won')
-                        print(f'Sorry {Player2}! You lost')
-                        player1_money+=bet1
-                        player2_money-=bet2
-                        print(f'{Player1} Your money is {player1_money}')
-                        print(f'{Player2} Your money is {player2_money}')
+                        p1_won()
+                        money()
                     elif guess1!=dice and guess2==dice:
-                        print(f'Congratulations {Player2}! You won')
-                        print(f'Sorry {Player1}! You lost')
-                        player1_money-=bet1
-                        player2_money+=bet2
-                        print(f'{Player1} Your money is {player1_money}')
-                        print(f'{Player2} Your money is {player2_money}')
+                        p2_won()
+                        money()
                     elif guess1==dice and guess2==dice:
-                        print(f'Congratulations {Player1} and {Player2}! You both won')
-                        player1_money+=bet1
-                        player2_money+=bet2
-                        print(f'{Player1} Your money is {player1_money}')
-                        print(f'{Player2} Your money is {player2_money}')
+                        Both_won()
+                        money()
                     else:
-                        print(f'Sorry {Player1} and {Player2}! You both lost')
-                        player1_money-=bet1
-                        player2_money-=bet2
-                        print(f'{Player1} Your money is {player1_money}')
-                        print(f'{Player2} Your money is {player2_money}')
+                        nobody_won()
+                        money()
                 else:
                     print('you dont have that kinda money \n Try again!')
                     game()
